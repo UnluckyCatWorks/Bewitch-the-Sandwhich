49cf17b3ce20b752338606305c73a0c28c6ee095
Vic <marsh.12th@gmail.com>
2018-03-23 13:52:16 +0100

    arreglaito un poco

--- Assets/Scripts/Management/Game.cs
+++ Assets/Scripts/Management/Game.cs
@@ -6,33 +6,24 @@ using System.Collections.Generic;
 public class Game : MonoBehaviour
 {
 	#region DATA
-	public Color[] teamColors;
-	public SmartAnimator aManager;
-
-	public static bool paused;		// Whether the game paused
 	public static Game manager;		// Self-reference
-	public static int[] scores;		// Score of both players
+	public static bool paused;      // Whether the game paused
+	public static RectTransform ui;
 
-	public static RectTransform ui; // Canvas (parent of all UI)
+	public Color[] teamColors;
 	#endregion
 
 	private void Update () 
 	{
 		OrderMaster.Update ();
-		if (Input.GetKeyDown (KeyCode.Q) && Input.GetKeyDown (KeyCode.LeftControl))
-			Application.Quit ();
 	}
 
 	private void Awake () 
 	{
-		// Keep master
-		DontDestroyOnLoad (gameObject);
 		// Initialize game
 		Marker.Initialize ();
 		OrderMaster.Initialize ();
 		DialogMaster.Initialize ();
-		ui = GameObject.Find ("UI").transform as RectTransform;
-		scores = new int[3];
 		manager = this;
 	}
 }
