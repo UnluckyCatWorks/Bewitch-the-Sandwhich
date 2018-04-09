c5e02c0261e6e7e39f69dc160b8a414abccb3516
Vic <marsh.12th@gmail.com>
2018-04-09 21:23:48 +0200

    bueno bueno

---
+++ Assets/Scenes/+ Tutorial/Scripts/Foco.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Foco : MonoBehaviour
+{
+	#region DATA
+	public const float speed = 3.0f;
+	public static Transform globalTarget;
+
+	public Transform target;
+	public bool bypass;
+	#endregion
+
+	private void Update () 
+	{
+		/// In-editor helper
+		if (bypass) return;
+
+		Transform follow;
+		if (globalTarget)	follow = globalTarget;
+		else if (target)	follow = target;
+		else				follow = null;
+		if (!follow) return;
+
+		/// Follow target
+		var newRot = Quaternion.LookRotation (follow.position - transform.position);
+		var lerp = Quaternion.Slerp (transform.rotation, newRot, speed * Time.deltaTime);
+		transform.rotation = lerp;
+	}
+}
